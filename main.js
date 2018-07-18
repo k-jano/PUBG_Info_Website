1a58c99942615d52f72f3dad61e2c28f2d058d56
kjano10 <kuba.jano3@gmail.com>
2018-07-18 22:50:15 +0200

    slider improved & footer added

--- main.js
+++ main.js
@@ -1,26 +1,82 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+var limit=0;
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+var timer1=0;
+var timer2=0;
+
+function hide(){
+  $("#slider").fadeOut(500);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
+function showDivs() {
+  var class_tmp="#badge"+slideIndex;
+  $(".badge").css('background-color', 'transparent');
+  $(class_tmp).css('background-color', 'white');
 
-function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+  limit=x.length;
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+  x[slideIndex].style.display = "block";
+
+  $("#slider").fadeIn(500);
+
+  timer1=setTimeout(function(){
+    calculateNum(slideIndex+=1);
+    showDivs();
+  }, 5000);
+  timer2=setTimeout("hide()", 4500);
 }
+      
+function currentDiv(n) {
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+
+  hide();
+  slideIndex=n;
+  setTimeout("showDivs()", 500);
+}
+
+function calculateNum(n){
+  if(n<0){n=limit-1};
+  if(n>limit-1){n=0};
+  slideIndex=n;
+}
+
+$(function(){
+  $( document ).ready(function(){
+    $("#badge0").css('background-color', 'white');
+  });
+
+  $(".badge").click(function(){
+    $(".badge").css('background-color', 'transparent');
+    $(this).css('background-color', 'white');
+  });
+    
+  $(".left").click(function(){
+    
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    hide();
+    calculateNum(slideIndex-=1);
+    setTimeout("showDivs()", 500);
+
+    var class_tmp="#badge"+slideIndex;
+    $(".badge").css('background-color', 'transparent');
+    $(class_tmp).css('background-color', 'white');
+  });
+
+  $(".right").click(function(){
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    hide();
+    calculateNum(slideIndex+=1);
+    setTimeout("showDivs()", 500);
+
+    var class_tmp="#badge"+slideIndex;
+    $(".badge").css('background-color', 'transparent');
+    $(class_tmp).css('background-color', 'white');
+  });
+});
