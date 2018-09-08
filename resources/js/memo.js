124a2f1a4903cac33d928e23e19c9e3aab8e52ff
kjano10 <kuba.jano3@gmail.com>
2018-09-08 23:30:11 +0200

    game works good, start and end screen to do

--- resources/js/memo.js
+++ resources/js/memo.js
@@ -4,16 +4,59 @@ var cards = ["AKM.png", "Groza.png", "Groza.png", "QBZ.png", "Scar-L.png", "AUG.
 $('.card').click(function(){
     var my_id=this.id;
     my_nr = my_id.substr(4, my_id.length - 1);
-    alert(my_nr);
+    revealCard(my_nr);
 });
 
 var oneVisible = false;
+var lock=false;
+var activeCard;
+var pairsLeft = 6;
+var counter=0;
 
 function revealCard(nr){
-    lock=true;
-    var img="url(../../img/memo/"+cards[nr]+")";
-    if(oneVisible==false){
-        oneVisible=true;
+    var thisOpacity = $('#card'+nr).css('opacity');
 
+    if(thisOpacity!=0 && lock==false && nr!=activeCard){
+
+        lock=true;
+        var my_img="url(img/memo/"+cards[nr]+")";
+        $('#card'+nr).css('background-image', my_img);
+        $('#card'+nr).addClass('cardActive');
+        $('#card'+nr).removeClass('card');
+        if(oneVisible==false){
+            oneVisible=true;
+            activeCard=nr;
+            lock=false
+        }
+        else{
+            if(cards[nr]==cards[activeCard]){
+                setTimeout(function(){
+                    $('#card'+nr).css('opacity',0);
+                    $('#card'+activeCard).css('opacity',0);
+                    
+                    pairsLeft--;
+                    if(pairsLeft==0)
+                        $('#board').html("<h1>Congrats! You won in " + counter + " turns</h1>");
+                    
+                    lock=false;
+                }, 1000);
+            }
+            else{
+                setTimeout(function(){
+                    $('#card'+nr).css('background-image', 'url(img/memo/back.png)');
+                    $('#card'+nr).addClass('card');
+                    $('#card'+nr).removeClass('cardActive');
+
+                    $('#card'+activeCard).css('background-image', 'url(img/memo/back.png)');
+                    $('#card'+activeCard).addClass('card');
+                    $('#card'+activeCard).removeClass('cardActive');
+
+                    lock=false;
+                }, 2000);
+            }
+            counter++;
+            $('#score').html("<p>Your score: "+ counter +"</p>");
+            oneVisible=false;
+        }
     }
 };
